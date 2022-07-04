35dbf07992bdab2cada86f4e298d4e95a96d8c0d
Lucacez <lucacespedes@protonmail.com>
2022-07-04 10:38:27 -0300

    commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,8 +6,8 @@ export const Contact = () => {
     <div>
       <p id='stack'>Contact Me</p>
       <label>you can contact me via mail: <br />lucacespedes00@gmail.com <br />You can also look for me on linkedin and github, there I upload what I'm working on lately.
-      <br /><a href="https://www.linkedin.com/in/lucacezdev/" target="_blank">https://www.linkedin.com/in/lucacezdev/</a><br /> 
-      <a href="https://github.com/Lucacez" target="_blank">https://github.com/Lucacez</a></label>
+      <br /><a href="https://www.linkedin.com/in/lucacezdev/" target="_blank" className='links'>https://www.linkedin.com/in/lucacezdev/</a><br /> 
+      <a href="https://github.com/Lucacez" target="_blank" className='links'>https://github.com/Lucacez</a></label>
     </div>
   )
 }
