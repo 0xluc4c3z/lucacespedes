5c6c9edfce67f9140e31ab3072da8eb1e23edd52
Lucacez <lucacespedes@protonmail.com>
2022-07-04 09:57:22 -0300

    commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import './Conctact.css'
 
 export const Contact = () => {
   return (
     <div>
       <p id='stack'>Contact Me</p>
       <label>you can contact me via mail: <br />lucacespedes00@gmail.com <br />You can also look for me on linkedin and github, there I upload what I'm working on lately.
-      <br /><a href="https://www.linkedin.com/in/lucacezdev/">https://www.linkedin.com/in/lucacezdev/</a><br /> <a href="https://github.com/Lucacez">https://github.com/Lucacez</a></label>
+      <br /><a href="https://www.linkedin.com/in/lucacezdev/" target="_blank">https://www.linkedin.com/in/lucacezdev/</a><br /> 
+      <a href="https://github.com/Lucacez" target="_blank">https://github.com/Lucacez</a></label>
     </div>
   )
 }
